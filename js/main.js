4629a43a090661b73714256ed7e59d02c06c9e20
Noelia-F <noelia.fronton.g@gmail.com>
2018-02-23 22:25:21 +0100

    show more info OK

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 'use strict';
+
+//funcionalidad menu
 var burger = document.querySelector('.header__button');
 var body = document.querySelector('.body');
+var menu = document.querySelector('.header__navigation');
 
 function showRemoveMenu(){
 	body.classList.toggle('menu-visible');
@@ -16,3 +19,27 @@ for(var i=0; i < items.length; i++) {
 }
 
 burger.addEventListener('click', showRemoveMenu);
+menu.addEventListener('click', hideMenu);
+
+//funcionalidad más info
+var more = document.querySelectorAll('.info--add');
+var addInfo = document.querySelectorAll('.aditional-info');
+
+for(var i=0; i<more.length; i++) {
+	more[i].addEventListener('click', showMore);
+	console.log(more);
+}
+//
+// for(var i=0; i<addInfo.length; i++) {
+// 	addInfo[i].addEventListener('click', showMore);
+// }
+
+function showMore(){
+	for(var i=0; i<addInfo.length; i++) {
+		addInfo[i].classList.toggle('hidden');
+	};
+	for(var i=0; i<more.length; i++) {
+		more[i].classList.toggle('info--less');
+		more[i].classList.toggle('info--plus');
+	}
+}
